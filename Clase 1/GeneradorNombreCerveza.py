90937439a808e7045893f8615e9cd788b0e552c9
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-08-01 14:44:14 +0200

    Commit-01

---
+++ Clase 1/GeneradorNombreCerveza.py
@@ -0,0 +1,1 @@
+print("La cerveza se llama:\n" "'"+(input("Cual es tu tipo de cerveza favorita?")) +" "+ (input("Que pais quieres visitar proximamente?"))+"'")
---
+++ Clase 1/index.py
@@ -0,0 +1,7 @@
+print(input("¿Qué estás estudiando?"))
+print('Me encanta estudiar python')
+print("Thi isn't a number")
+print ('This isn\'t a number')
+print (input ("Dime tu nombre"))
+print ("Tu nombre es" +" " +input ("Dime tu nombre")+"\nTu apellido es " +input ("Dime tu Apellido "))
+
